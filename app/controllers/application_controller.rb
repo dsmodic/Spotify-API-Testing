2d71572ad7bef979ad4186e9f06a0444a60ab931
dsmodic <dsmodic@chicagobooth.edu>
2020-11-25 02:18:39 +0000

    Implemented Spotify API Gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+
+  
+  
+
+  def index
+
+    require 'rspotify'
+    RSpotify.authenticate(ENV.fetch("Spotify_Client_ID"), ENV.fetch("Spotify_Client_Secret"))
+    render({ :template => "index.html.erb"})
+  end
+
 end
