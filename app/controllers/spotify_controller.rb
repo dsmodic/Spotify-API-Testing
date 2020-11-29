cdf9e5289c59e40465289c8c3b3d764c1f99752a
dsmodic <dsmodic@chicagobooth.edu>
2020-11-29 04:30:12 +0000

    Trivial Update

--- app/controllers/spotify_controller.rb
+++ app/controllers/spotify_controller.rb
@@ -1,6 +1,7 @@
 class SpotifyController < ApplicationController
   before_action(:spotify_authentication)
 
+  #SPOTIFY APP LOGIN
   def spotify_authentication
     require 'rspotify'
     RSpotify.authenticate(ENV.fetch("Spotify_Client_ID"), ENV.fetch("Spotify_Client_Secret"))
